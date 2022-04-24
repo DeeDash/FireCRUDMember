4d3e6546deb1522541900f119a7e88f48ca1feb8
DeeDash <rumpelstiltskin512@gmail.com>
2022-04-24 13:34:19 +0530

    fix register

--- app/src/main/java/com/deedash/firecrud/RegisterActivity.java
+++ app/src/main/java/com/deedash/firecrud/RegisterActivity.java
@@ -41,48 +41,42 @@ public class RegisterActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         //adding on click for login tv.
-        loginTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //opening a login activity on clicking login text.
-                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(i);
-            }
+        loginTV.setOnClickListener(v -> {
+            //opening a login activity on clicking login text.
+            Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(i);
         });
         //adding click listener for register button.
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //hiding our progress bar.
-                loadingPB.setVisibility(View.VISIBLE);
-                //getting data fro =m our edit text.
-                String userName = userNameEdt.getText().toString();
-                String pwd = passwordEdt.getText().toString();
-                String cnfPwd = confirmPwdEdt.getText().toString();
-                //checking if the password and confirm password is equal or not.
-                if (!pwd.equals(cnfPwd)) {
-                    Toast.makeText(RegisterActivity.this, "Please check both having same password..", Toast.LENGTH_SHORT).show();
-                } else if (TextUtils.isEmpty(userName) && TextUtils.isEmpty(pwd) && TextUtils.isEmpty(cnfPwd)) {
-                    //checking if the text fields are empty or not.
-                    Toast.makeText(RegisterActivity.this, "Please enter your credentials..", Toast.LENGTH_SHORT).show();
-                } else {
-                    //on below line we are creating a new user by passing email and password.
-                    mAuth.createUserWithEmailAndPassword(userName, pwd).addOnCompleteListener(task -> {
-                        //on below line we are checking if the task is success or not.
-                        if (task.isSuccessful()) {
-                            //in on success method we are hiding our progress bar and opening a login activity.
-                            loadingPB.setVisibility(View.GONE);
-                            Toast.makeText(RegisterActivity.this, "User Registered..", Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
-                            startActivity(i);
-                            finish();
-                        } else {
-                            //in else condition we are displaying a failure toast message.
-                            loadingPB.setVisibility(View.GONE);
-                            Toast.makeText(RegisterActivity.this, "Fail to register user..", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
+        registerBtn.setOnClickListener(v -> {
+            //hiding our progress bar.
+            loadingPB.setVisibility(View.VISIBLE);
+            //getting data fro =m our edit text.
+            String userName = userNameEdt.getText().toString();
+            String pwd = passwordEdt.getText().toString();
+            String cnfPwd = confirmPwdEdt.getText().toString();
+            //checking if the password and confirm password is equal or not.
+            if (!pwd.equals(cnfPwd)) {
+                Toast.makeText(RegisterActivity.this, "Please check both having same password..", Toast.LENGTH_SHORT).show();
+            } else if (TextUtils.isEmpty(userName) && TextUtils.isEmpty(pwd) && TextUtils.isEmpty(cnfPwd)) {
+                //checking if the text fields are empty or not.
+                Toast.makeText(RegisterActivity.this, "Please enter your credentials..", Toast.LENGTH_SHORT).show();
+            } else {
+                //on below line we are creating a new user by passing email and password.
+                mAuth.createUserWithEmailAndPassword(userName, pwd).addOnCompleteListener(task -> {
+                    //on below line we are checking if the task is success or not.
+                    if (task.isSuccessful()) {
+                        //in on success method we are hiding our progress bar and opening a login activity.
+                        loadingPB.setVisibility(View.GONE);
+                        Toast.makeText(RegisterActivity.this, "User Registered..", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(i);
+                        finish();
+                    } else {
+                        //in else condition we are displaying a failure toast message.
+                        loadingPB.setVisibility(View.GONE);
+                        Toast.makeText(RegisterActivity.this, "Fail to register user..", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
